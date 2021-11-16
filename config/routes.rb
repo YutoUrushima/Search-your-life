9baab68ebe6dd3720c593f67d4422b117f3182c3
EC2 Default User <ec2-user@ip-172-31-17-217.ap-northeast-1.compute.internal>
2021-11-16 08:33:06 +0000

    rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 #   get 'lifes/new'
 #   get 'lifes/edit'
 
+root "lifes#index"
 resources :lifes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
