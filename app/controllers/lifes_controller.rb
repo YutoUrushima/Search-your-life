e63dbf79a53a5c390aa91864ec5f9f1578f9fe2e
EC2 Default User <ec2-user@ip-172-31-17-217.ap-northeast-1.compute.internal>
2021-12-06 08:16:30 +0000

    ログイン機能

--- app/controllers/lifes_controller.rb
+++ app/controllers/lifes_controller.rb
@@ -3,6 +3,19 @@ class LifesController < ApplicationController
   def login_form
   end
   
+  def login
+    @mail = params[:mail]
+    @password = params[:password]
+    @user = Life.find_by(mail: params[:mail])
+    if @user &&  @user.password == @password
+      flash[:notice] = "Success!"
+      redirect_to "/"
+    else
+      flash[:alert] = "Error!"
+      render "login_form"
+    end
+  end
+  
   def check
     @email = params[:mail]
     @password = params[:password]
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ root "lifes#index"
 resources :lifes
 
 get "/login" => "lifes#login_form"
-post "/login/autentication" => "lifes#check"
+post "/login" => "lifes#login"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
