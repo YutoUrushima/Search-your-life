9dc0db1f93ee8f2ad6a98c60c85da0806ae2dca8
EC2 Default User <ec2-user@ip-172-31-17-217.ap-northeast-1.compute.internal>
2021-12-10 07:47:54 +0000

    人生イベントの編集&削除

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -13,6 +13,28 @@ class ContentsController < ApplicationController
   end
   
   def edit
+    @life = Life.find(params[:life_id])
+    @content = @life.contents.find(params[:id])
+  end
+  
+  def update
+    @life = Life.find(params[:life_id])
+    @content = @life.contents.find(params[:id])
+    if @content.update(content_params)
+      redirect_to life_path(@life)
+      flash[:notice] = "Success!"
+    else
+      render :edit
+      flash[:alert] = "Error!"
+    end
+  end
+  
+  def destroy
+    @life = Life.find(params[:life_id])
+    @content = @life.contents.find(params[:id])
+    @content.destroy
+    redirect_to life_path(@life)
+    flash[:notice] = "Deleted!"
   end
   
   private
