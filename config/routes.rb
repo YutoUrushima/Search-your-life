6b99eafcaa255ec6f0a313646018030df870a34b
YutoUrushima <yuto259233@gmail.com>
2022-01-14 07:52:10 +0000

    routesファイルの整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
   get 'contents/edit'
-#   get 'lifes/index'
-#   get 'lifes/show'
-#   get 'lifes/new'
-#   get 'lifes/edit'
-
-get "/login", to: "lifes#login_form"
-post "/login", to: "lifes#login"
-post "/logout", to: "lifes#logout"
-
-root 'lifes#index'
-resources :lifes do
-  resources :contents
-end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/login", to: "lifes#login_form"
+  post "/login", to: "lifes#login"
+  post "/logout", to: "lifes#logout"
+  root 'lifes#index'
+  resources :lifes do
+    resources :contents
+  end
 end
