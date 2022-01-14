c05e717bf4763236ec0ae8913b023165b3adc1fe
YutoUrushima <yuto259233@gmail.com>
2022-01-14 08:39:14 +0000

    静的ページのテスト

--- test/controllers/lifes_controller_test.rb
+++ test/controllers/lifes_controller_test.rb
@@ -1,18 +1,26 @@
 require 'test_helper'
 
 class LifesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @common_title = "Search your life"
+  end
+  
   test "should get index" do
     get root_path
     assert_response :success
+    assert_select "title", "Home | #{@common_title}"
   end
 
   test "should get new" do
     get new_life_path
     assert_response :success
+    assert_select "title", "New | #{@common_title}"
   end
   
   test "should get login" do
     get login_path
     assert_response :success
+    assert_select "title", "Login | #{@common_title}"
   end
 end
