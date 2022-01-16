5eadc8564a30d335e0ca97e5736a39e5cb190d83
YutoUrushima <yuto259233@gmail.com>
2022-01-16 09:17:01 +0000

    ルーティングファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'lifes#index'
   get 'contents/edit'
   get "/login", to: "lifes#login_form"
   post "/login", to: "lifes#login"
   post "/logout", to: "lifes#logout"
-  root 'lifes#index'
+  
   resources :lifes do
     resources :contents
   end
