82d4258a54bbda7d4600c3f4e87748901271a60d
YutoUrushima <yuto259233@gmail.com>
2022-01-16 09:45:26 +0000

    メールアドレスの存在性のテスト

--- test/models/life_test.rb
+++ test/models/life_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class LifeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @life = Life.new(mail: "example@example.com", password: "password", password_confirmation: "password")
+  end
+  
+  # ユーザーの妥当性のテスト
+  test "should be valid" do
+    assert @life.valid?
+  end
+  
+  # ユーザーのメールアドレスは存在する必要がある
+  test "mail should be present" do
+    @life.mail = ""
+    assert_not @life.valid?
+  end
 end
