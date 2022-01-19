b64433a1a9e20a5b65c05d97f5bda81225f07434
YutoUrushima <yuto259233@gmail.com>
2022-01-19 08:07:07 +0000

    seedファイルの読み込み直し

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Life.create(name: "uru", year: 1996, month: 12, date: 20, introduce: "Developer", image: "https://twitter.com/developer_japan/status/1444432857342955520/photo/1", email: "exmaple@example.co.jp", password_digest: "1220")
-# Life.create(name: "taro", year: 1995, month: 8, date: 10, introduce: "image", image: "https://twitter.com/developer_japan/status/1443895899185319940/photo/1", email: "exmaple2@example2.co.jp", password_digest: "1996")
+Life.create(name: "uru", year: 1996, month: 12, date: 20, introduce: "Developer", image: "https://twitter.com/developer_japan/status/1444432857342955520/photo/1", email: "exmaple@example.co.jp", password: "1220", password_confirmation: "1220")
