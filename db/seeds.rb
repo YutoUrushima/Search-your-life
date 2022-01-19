e7bfdd0a9159c606b8544897ce3839a4719e0898
YutoUrushima <yuto259233@gmail.com>
2022-01-19 08:09:15 +0000

    seedファイルの整理

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Life.create(name: "uru", year: 1996, month: 12, date: 20, introduce: "Developer", image: "https://twitter.com/developer_japan/status/1444432857342955520/photo/1", email: "exmaple@example.co.jp", password: "1220", password_confirmation: "1220")
+Life.create!(name: "uru", year: 1996, month: 12, date: 20, introduce: "Developer", image: "https://twitter.com/developer_japan/status/1444432857342955520/photo/1", email: "exmaple@example.co.jp", password: "1220", password_confirmation: "1220")
