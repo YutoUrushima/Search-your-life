989569c800dbbc50c07db5d8c5de29a87dabdf53
YutoUrushima <yuto259233@gmail.com>
2022-01-25 11:47:08 +0000

    名前のバリデーション

--- app/models/life.rb
+++ app/models/life.rb
@@ -3,6 +3,7 @@ class Life < ApplicationRecord
     before_save {self.email = self.email.downcase}
     # https://railstutorial.jp/chapters/modeling_users?version=6.0#sec-format_validation
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :name, presence: true
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: {minimum: 6}
     has_secure_password
--- test/integration/life_registration_test.rb
+++ test/integration/life_registration_test.rb
@@ -9,7 +9,7 @@ class LifeRegistrationTest < ActionDispatch::IntegrationTest
   test "valid new life registration" do
     get new_life_path
     assert_template 'new'
-    post lifes_path, params: {life: {email: "abc@exmaple.com", password: "foobar", password_confirmation: "foobar"}}
+    post lifes_path, params: {life: {name: "うる", email: "abc@exmaple.com", password: "foobar", password_confirmation: "foobar"}}
     assert_redirected_to root_path
     assert_not flash.empty?
     # ユーザーの基本情報の登録
