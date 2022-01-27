b386dbdfd899fee7e059b69ab73d21e4c6182b21
YutoUrushima <yuto259233@gmail.com>
2022-01-27 07:57:23 +0000

    ユーザー登録のテスト

--- test/integration/life_registration_test.rb
+++ test/integration/life_registration_test.rb
@@ -6,11 +6,24 @@ class LifeRegistrationTest < ActionDispatch::IntegrationTest
     @life = lives(:new_life)
   end
   
-  test "valid new life registration" do
+  # 有効な情報とともにユーザー登録
+  test "registration with valid information" do
     get new_life_path
-    post lifes_path, params: {life: {name: "うる", email: "abc@exmaple.com", password: "foobar", password_confirmation: "foobar"}}
+    assert_difference 'Life.count', 1 do
+      post lifes_path, params: {life: {name: "うる", email: "abc@exmaple.com", password: "foobar", password_confirmation: "foobar"}}
+    end
     assert_redirected_to root_path
     follow_redirect!
+    assert_not flash.empty?
+  end
+  
+  # 無効な情報とともにユーザー登録
+  test "registration with invalid information" do
+    get new_life_path
+    assert_no_difference 'Life.count' do
+      post lifes_path, params: {life: {name: "", email: "abc@exmaple.com", password: "foobar", password_confirmation: "foobaz"}}
+    end
+    assert_template 'new'
     assert_not flash.empty?
   end
 end
