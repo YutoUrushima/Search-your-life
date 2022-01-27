eebfd89a411bf224c652cc371f93293b35ce41dc
YutoUrushima <yuto259233@gmail.com>
2022-01-27 07:07:46 +0000

    プロフィールのアップデートの際にパスワードにもバリデートがかかってる #1

--- app/models/life.rb
+++ app/models/life.rb
@@ -5,8 +5,8 @@ class Life < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :name, presence: true
     validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: {minimum: 6}
     has_secure_password
+    validates :password, presence: true, length: {minimum: 6}, allow_nil: true
     
     # 渡されたパスワード（文字列）のハッシュ値を返す
     # https://railstutorial.jp/chapters/basic_login?version=6.0#sec-testing_layout_changes
