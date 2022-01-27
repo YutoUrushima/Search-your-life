e42b3e201370bd995565c1480307e6c8a5e8efa1
YutoUrushima <yuto259233@gmail.com>
2022-01-27 08:05:13 +0000

    seedのパスワードno

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Life.create!(name: "uru", year: 1996, month: 12, date: 20, introduce: "Developer", image: "https://twitter.com/developer_japan/status/1444432857342955520/photo/1", email: "exmaple@example.co.jp", password: "1220", password_confirmation: "1220")
+Life.create!(name: "うる", year: 1996, month: 12, date: 20, introduce: "Developer", image: "https://twitter.com/developer_japan/status/1444432857342955520/photo/1", email: "exmaple@example.co.jp", password: "123456", password_confirmation: "123456")
