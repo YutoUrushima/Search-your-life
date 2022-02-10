e465d6fa22a1d99f25b59ec107c4df8ae8a1d0c6
YutoUrushima <yuto259233@gmail.com>
2022-02-10 09:23:39 +0000

    現在表示中のページのユーザーのidとログイン中のユーザーidがあってるか確認のショートコード

--- app/helpers/lifes_helper.rb
+++ app/helpers/lifes_helper.rb
@@ -1,6 +1,13 @@
 module LifesHelper
+    include SessionsHelper
+    
     # ユーザーのプロフィール画像を返却する
     def profile_image(life)
         life.image != nil ? life.image : "/user_images/default.jpg"
+    end
+    
+    # 現在表示中のページのユーザーのidとログイン中のユーザーidがあってるか確認(showページでの編集ボタンなどの表示確認用)
+    def match_correct_life?(life)
+        current_user && life.id == current_user.id
     end
 end
